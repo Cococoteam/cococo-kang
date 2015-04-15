0610142280425e50986ff99caa1f898c74b62f2e
sunghyun <sunghyun@117.16.197.169>
2015-04-15 16:16:41 +0900

    블루투스 on 및 블루투스 연결, 아두이노 데이터 값 한개 보내기

--- gen/com/example/rec/R.java
+++ gen/com/example/rec/R.java
@@ -469,72 +469,82 @@ containing a value of this type.
         public static final int selectexample=0x7f020025;
     }
     public static final class id {
-        public static final int RecUI=0x7f0b002c;
-        public static final int action_settings=0x7f0b0035;
+        public static final int RecUI=0x7f0b0033;
+        public static final int action_settings=0x7f0b003c;
         public static final int adjust_height=0x7f0b0000;
         public static final int adjust_width=0x7f0b0001;
+        public static final int bluetooth=0x7f0b002a;
         public static final int book_now=0x7f0b0010;
-        public static final int btn=0x7f0b0034;
+        public static final int btn=0x7f0b003b;
+        public static final int button_scan=0x7f0b001c;
         public static final int buyButton=0x7f0b000c;
         public static final int buy_now=0x7f0b0011;
         public static final int buy_with_google=0x7f0b0012;
-        public static final int chronometer1=0x7f0b0030;
+        public static final int chronometer1=0x7f0b0037;
         public static final int classic=0x7f0b0014;
-        public static final int connection=0x7f0b0024;
-        public static final int control=0x7f0b0025;
+        public static final int connection=0x7f0b002c;
         public static final int donate_with_google=0x7f0b0013;
-        public static final int editText1=0x7f0b0018;
-        public static final int filelist_ui=0x7f0b0017;
-        public static final int filename=0x7f0b001f;
-        public static final int graphView=0x7f0b002e;
+        public static final int editText1=0x7f0b001e;
+        public static final int filelist_ui=0x7f0b001d;
+        public static final int filename=0x7f0b0025;
+        public static final int graphView=0x7f0b0035;
         public static final int grayscale=0x7f0b0015;
         public static final int holo_dark=0x7f0b0007;
         public static final int holo_light=0x7f0b0008;
         public static final int hybrid=0x7f0b0003;
-        public static final int imageView1=0x7f0b0021;
-        public static final int img=0x7f0b0032;
-        public static final int list=0x7f0b0019;
-        public static final int main=0x7f0b0020;
-        public static final int map=0x7f0b0027;
+        public static final int imageView1=0x7f0b0027;
+        public static final int img=0x7f0b0039;
+        public static final int list=0x7f0b001f;
+        public static final int listView=0x7f0b0017;
+        public static final int main=0x7f0b0026;
+        public static final int map=0x7f0b002e;
         public static final int match_parent=0x7f0b000e;
-        public static final int mediaplay_ui=0x7f0b0028;
-        public static final int message=0x7f0b001e;
+        public static final int mediaplay_ui=0x7f0b002f;
+        public static final int message=0x7f0b0024;
         public static final int monochrome=0x7f0b0016;
+        public static final int new_devices=0x7f0b001b;
         public static final int none=0x7f0b0002;
         public static final int normal=0x7f0b0004;
-        public static final int password=0x7f0b001b;
-        public static final int play=0x7f0b0029;
+        public static final int paired_devices=0x7f0b0019;
+        public static final int password=0x7f0b0021;
+        public static final int play=0x7f0b0030;
         public static final int production=0x7f0b0009;
-        public static final int progress=0x7f0b002b;
-        public static final int receivemail=0x7f0b001c;
-        public static final int recorded=0x7f0b0031;
-        public static final int recording=0x7f0b0022;
+        public static final int progress=0x7f0b0032;
+        public static final int receivemail=0x7f0b0022;
+        public static final int recorded=0x7f0b0038;
+        public static final int recording=0x7f0b0028;
         public static final int sandbox=0x7f0b000a;
         public static final int satellite=0x7f0b0005;
-        public static final int select=0x7f0b0023;
+        public static final int search=0x7f0b002b;
+        public static final int select=0x7f0b0029;
         public static final int selectionDetails=0x7f0b000d;
-        public static final int sendemail=0x7f0b001a;
-        public static final int setting=0x7f0b0026;
-        public static final int stop=0x7f0b002a;
+        public static final int sendemail=0x7f0b0020;
+        public static final int setting=0x7f0b002d;
+        public static final int stop=0x7f0b0031;
         public static final int strict_sandbox=0x7f0b000b;
         public static final int terrain=0x7f0b0006;
-        public static final int text=0x7f0b0033;
-        public static final int textView1=0x7f0b002d;
-        public static final int textView2=0x7f0b002f;
-        public static final int title=0x7f0b001d;
+        public static final int text=0x7f0b003a;
+        public static final int textView1=0x7f0b0034;
+        public static final int textView2=0x7f0b0036;
+        public static final int title=0x7f0b0023;
+        public static final int title_new_devices=0x7f0b001a;
+        public static final int title_paired_devices=0x7f0b0018;
         public static final int wrap_content=0x7f0b000f;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f080000;
     }
     public static final class layout {
-        public static final int filelist=0x7f030000;
-        public static final int mailform=0x7f030001;
-        public static final int mainui=0x7f030002;
-        public static final int mapui=0x7f030003;
-        public static final int mediaplay=0x7f030004;
-        public static final int recui=0x7f030005;
-        public static final int view=0x7f030006;
+        public static final int bluetoothlist=0x7f030000;
+        public static final int device_list=0x7f030001;
+        public static final int device_name=0x7f030002;
+        public static final int filelist=0x7f030003;
+        public static final int mailform=0x7f030004;
+        public static final int mainui=0x7f030005;
+        public static final int mapui=0x7f030006;
+        public static final int mediaplay=0x7f030007;
+        public static final int recui=0x7f030008;
+        public static final int view=0x7f030009;
     }
     public static final class menu {
         public static final int rec=0x7f0a0000;
@@ -546,6 +556,8 @@ containing a value of this type.
         public static final int accept=0x7f060002;
         public static final int action_settings=0x7f060025;
         public static final int app_name=0x7f060023;
+        public static final int bt_not_enabled_leaving=0x7f060033;
+        public static final int button_scan=0x7f06003b;
         /**   Text for notification shown when the Android Wear apps needs to be updated. Only shown on the wearable. [CHAR LIMIT=NONE] 
          */
         public static final int common_android_wear_notification_needs_update_text=0x7f060009;
@@ -648,7 +660,9 @@ containing a value of this type.
         public static final int create_calendar_message=0x7f060005;
         public static final int create_calendar_title=0x7f060004;
         public static final int decline=0x7f060003;
+        public static final int discoverable=0x7f06003e;
         public static final int hello_world=0x7f060024;
+        public static final int insecure_connect=0x7f06003d;
         public static final int mainui_connection_title=0x7f060028;
         public static final int mainui_control_title=0x7f060029;
         public static final int mainui_recording_title=0x7f060026;
@@ -656,12 +670,28 @@ containing a value of this type.
         public static final int mainui_setting_title=0x7f06002a;
         public static final int mediaplay_play_btn=0x7f06002f;
         public static final int mediaplay_stop_btn=0x7f060030;
+        public static final int none_found=0x7f060038;
+        public static final int none_paired=0x7f060037;
+        public static final int not_connected=0x7f060032;
         public static final int recui_play_btn=0x7f06002d;
         public static final int recui_recording_btn=0x7f06002c;
         public static final int recui_recording_title=0x7f06002b;
         public static final int recui_time_title=0x7f06002e;
+        /**   DeviceListActivity 
+         */
+        public static final int scanning=0x7f060035;
+        /**  Options Menu 
+         */
+        public static final int secure_connect=0x7f06003c;
+        public static final int select_device=0x7f060036;
+        /**   BluetoothChat 
+         */
+        public static final int send=0x7f060031;
         public static final int store_picture_message=0x7f060001;
         public static final int store_picture_title=0x7f060000;
+        public static final int title_connecting=0x7f060034;
+        public static final int title_other_devices=0x7f06003a;
+        public static final int title_paired_devices=0x7f060039;
         /**  Text on a placeholder buy button when Google Play services is not
          available or up-to-date 
          */
